fcf16f930df7966a036a7d1e43c7516de9cd16e1
vdoukadoukopoulou <doukavictoria@gmail.com>
2020-04-25 22:41:52 +0200

    stef saw my program and now it has 21 new lines and the documentation is obsolete
    #winning #idoubtit

--- birdcam.py
+++ birdcam.py
@@ -1,18 +1,26 @@
+from dotenv import load_dotenv
+import os
 import schedule
 import time
 from goprohero import GoProHero
 
+localtime = time.asctime( time.localtime(time.time()) )
+print(localtime,"I set up camera...")
+load_dotenv()
+your_password = os.getenv("KEY")
+camera = GoProHero(password=your_password)
+print(localtime,"I did set up camera...")
+
 def job():
-    print("I'm working...")
-    camera = GoProHero(password='florentijn')
-    print("I'm doing job...")
+    print(localtime,"I'm working...")
+    print(localtime,"I'm doing job...")
     camera.command('power', 'on')
     time.sleep(30)
     camera.command('record', 'on')
     time.sleep(7200)
     camera.command('record', 'off')
     camera.command('power', 'sleep')
-    print("I did job...")
+    print(localtime,"I did job...")
 
 schedule.every().day.at("05:30").do(job)
 
